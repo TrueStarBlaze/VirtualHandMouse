e9bd25cd7bf4a08b4f813ea2e1850a7c43b5de36
erick <erick@10.0.0.46>
2022-01-26 11:22:35 -0700

    Prototype with javacv lib

---
+++ src/virtualhandmouseprototypev/pkg1/HandTrendAnalyzer.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package virtualhandmouseprototypev.pkg1;
+
+/**
+ *
+ * @author erick
+ */
+public class HandTrendAnalyzer {
+    NLinkedList handLocs;
+    public static void updateHandLocList(){
+        
+    }
+    public static int[] determineTrend(int[] handLoc){
+        return null;
+    }
+}
---
+++ src/virtualhandmouseprototypev/pkg1/VideoAccessor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package virtualhandmouseprototypev.pkg1;
+
+    import org.bytedeco.javacv.FrameGrabber;
+
+/**
+ *
+ * @author erick
+ */
+public class VideoAccessor  {
+   
+        public java.awt.Image getFrame() {
+            FrameGrabber fg = new FrameGrabber();
+            
+            return null;
+        }
+}
