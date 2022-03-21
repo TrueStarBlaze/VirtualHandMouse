90dfaa8e1e8bc4e80d9d31c8a9af3ea2bff09777
erick <erick@10.0.0.46>
2022-03-21 17:38:16 -0600

    FingerCount work

--- src/main/java/com/mycompany/virtualhandmouseprototype/FingerCount.java
+++ src/main/java/com/mycompany/virtualhandmouseprototype/FingerCount.java
@@ -5,7 +5,15 @@
  */
 package com.mycompany.virtualhandmouseprototype;
 
+import java.util.ArrayList;
+import org.bytedeco.opencv.global.opencv_core;
+import org.bytedeco.opencv.opencv_core.Mat;
+import org.bytedeco.opencv.opencv_core.Point;
 import org.bytedeco.opencv.opencv_core.Scalar;
+import org.bytedeco.opencv.global.opencv_imgproc;
+//import org.bytedeco.opencv.presets.opencv_imgproc
+import org.bytedeco.opencv.opencv_core.MatVector;
+import org.bytedeco.opencv.opencv_core.MatVectorVector;
 
 /**
  *
@@ -19,12 +27,33 @@ public class FingerCount {
     private final Scalar black = new Scalar(0, 0, 0, 0);
     private final Scalar white = new Scalar(255, 255, 255, 0);
     private final Scalar yellow = new Scalar(0, 255, 255, 0);
-    private final Scalar purple = new Scalar(255, 0, 255, 0); 
-    /*
-	color_red = Scalar(0, 0, 255);
-	color_black = Scalar(0, 0, 0);
-	color_white = Scalar(255, 255, 255);
-	color_yellow = Scalar(0, 255, 255);
-	color_purple = Scalar(255, 0, 255);
-    */
+    private final Scalar purple = new Scalar(255, 0, 255, 0);
+    
+    public Mat findFingersCount(Mat input, Mat frame){
+        Mat contoursMat = Mat.zeros(input.size(), opencv_core.CV_8UC3).asMat();
+        
+        if (input.empty() || input.channels() != 1) {
+            return contoursMat;
+        }
+        MatVector contours = null;
+        Mat hierarchy = null;
+        opencv_imgproc.findContours(input, contours, hierarchy, opencv_imgproc.CV_RETR_EXTERNAL, opencv_imgproc.CHAIN_APPROX_NONE);
+        
+        if (contours.size() <= 0) {
+            return contoursMat;
+        }
+        int biggestContourIdx = -1;
+        double biggestArea = 0;
+        for (int i = 0; i < contours.size(); ++i) {
+            Mat[] contoursMatArr = contours.get();
+            double area = opencv_imgproc.contourArea(contoursMatArr[i], true);
+            if (area > biggestArea) {
+                biggestArea = area;
+                biggestContourIdx = i;
+            }
+        }
+        
+        
+    }
+    
 }
